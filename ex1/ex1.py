eb87273b3f9e0e5bf584fdce40b9be5c270bd08f
yan0606 <yan060604@gmail.com>
2024-04-22 10:34:19 -0300

    :sparkles: exercise 1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -19,14 +19,10 @@ class Produto:
         return f"Nome: {self.nome}, Descrição: {self.descricao}, Quantidade: {self.quantidade}, Valor: R${self.valor:.2f}"
 
 
-# Exemplo de uso das classes
 if __name__ == "__main__":
-    # Criando um usuário
     usuario1 = Usuario("yan", "yan@email.com", "senha123")
 
-    # Criando um produto
     produto1 = Produto("Notebook", "Notebook Acer", 10, 4500.50)
 
-    # Imprimindo informações do usuário e produto
     print(usuario1)
     print(produto1)
